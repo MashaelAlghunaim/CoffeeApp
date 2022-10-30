ac9a6e3ed46fa5f454100a16ef01905472a18aa9
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-30 09:40:03 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Mashael Alghunaim on 05/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
